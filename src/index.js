d53812fe11726dd922e95a755211375aab782759
syedmohsinnkarim <syedmohsinnkarim@gmail.com>
2020-04-30 01:56:01 +0500

    'node-api'

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import cors from "cors";
+import express from "express";
+import connect from "./database";
+import compression from "compression";
+import helmet from "helmet";
+import { routes as mylinks } from "./links";
+
+const app = express();
+
+// Application Level Middlewares
+app.use(helmet());
+app.use(compression());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/link", mylinks);
+
+// Database Connection with host
+connect()
+  .then(async () =>
+    app.listen(process.env.PORT, () =>
+      console.log(`Example app listening on port ${process.env.PORT}!`)
+    )
+  )
+  .catch(error => {
+    console.log(`Error is found ${error}`);
+  });
